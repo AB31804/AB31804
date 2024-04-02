3571fb100e3e6804b287e25f25395dfb4bf64b9f
AB31804 <156466931+AB31804@users.noreply.github.com>
2024-04-02 10:42:30 -0400

    added click event
    click event changes the big image

--- Assignment-4/Part-2/main.js
+++ Assignment-4/Part-2/main.js
@@ -5,11 +5,9 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
 const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
-
 const texts = {
     'pic1.jpg': 'Closeup of a human eye',
     'pic2.jpg': 'Rock that looks like a seashell',
@@ -24,6 +22,10 @@ for (const image of images){
     newImage.setAttribute('src', `images/${image}`);
     newImage.setAttribute('alt', texts[image]);
     thumbBar.appendChild(newImage);
+    newImage.addEventListener('click',(event)=>{
+        displayedImage.src = event.target.src;
+        displayedImage.alt = event.target.alt;
+    });
 }
 
 /* Wiring up the Darken/Lighten button */
