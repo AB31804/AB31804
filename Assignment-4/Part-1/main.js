d46cdfd7c48f28315067f1fe9e6cd241dd97d083
AB31804 <156466931+AB31804@users.noreply.github.com>
2024-04-02 09:43:59 -0400

    replaced bob and conversions
    replaced more parts of the story string

--- Assignment-4/Part-1/main.js
+++ Assignment-4/Part-1/main.js
@@ -34,13 +34,14 @@ function result() {
 
     if(customName.value !== '') {
         const name = customName.value;
-
+        newStory = newStory.replaceAll('Bob', name)
     }
 
     if(document.getElementById("uk").checked) {
-        const weight = Math.round(300);
-        const temperature =  Math.round(94);
-
+        const weight = `${Math.round(300 * 0.0714286)} stone`;
+        const temperature =  `${Math.round((94 - 32) * 5/9)} centigrade`;
+        newStory = newStory.replaceAll('94 fahrenheit', weight)
+        newStory = newStory.replaceAll('300 pounds', temperature)
     }
 
     story.textContent = ;
