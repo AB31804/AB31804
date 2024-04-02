aef03f4ed895c0c1fd96f8a038000785deec8372
AB31804 <156466931+AB31804@users.noreply.github.com>
2024-04-02 10:15:09 -0400

    declared variables
    added alternate text and image file name constants

--- Assignment-4/Part-2/main.js
+++ Assignment-4/Part-2/main.js
@@ -6,7 +6,17 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+
 /* Declaring the alternative text for each image file */
+
+const texts = {
+    'pic1.jpg': 'Closeup of a human eye',
+    'pic2.jpg': 'Rock that looks like a seashell',
+    'pic3.jpg': 'Purple and white flower',
+    'pic4.jpg': 'Egyptian Hyroglyphics',
+    'pic5.jpg': 'Butterfly on a leaf'
+}
 
 /* Looping through images */
 
