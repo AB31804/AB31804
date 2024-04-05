b4cf1a764380a7cf0071eed7b8fba94abd3640d3
AB31804 <156466931+AB31804@users.noreply.github.com>
2024-04-05 19:51:54 -0400

    fixed methods
    fixed methods and added ball animation loop

--- Assignment-4/Part-3/main.js
+++ Assignment-4/Part-3/main.js
@@ -27,13 +27,34 @@ class Ball {
     this.color = color;
     this.size = size;
   }
-}
+  
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 
-draw(); {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
 }
 
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
@@ -42,23 +63,34 @@ testBall.size;
 testBall.color;
 testBall.draw();
 
-update(); {
-  if ((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
+const balls = [];
 
-  if ((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
 
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-
-  if ((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
+  balls.push(ball);
 }
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
